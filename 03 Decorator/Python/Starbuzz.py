cb53dcef763ef4bc5dac44e97894fc2dfab7f778
Niko <nikolaus.naredi-rainer@zuehlke.com>
2022-06-11 15:09:34 +0200

    with initial changesto accomodate sizes

--- 03 Decorator/Python/Starbuzz.py
+++ 03 Decorator/Python/Starbuzz.py
@@ -1,4 +1,11 @@
 from abc import ABC
+from enum import Enum
+
+
+class Size(Enum):
+    Tall = 1
+    Grande = 2
+    Venti = 3
 
 
 class Beverage(ABC):
@@ -8,12 +15,19 @@ class Beverage(ABC):
 
     def __init__(self):
         self.description = "unknown beverage"
+        self.size = Size.Tall
 
     def get_description(self) -> str:
         return self.description
 
     def cost(self) -> float:
         pass
+
+    def set_size(self, beverage_size: Size) -> None:
+        self.size = beverage_size
+
+    def get_size(self) -> Size:
+        return self.size
 
 
 class CondimentDecorator(Beverage):
@@ -37,12 +51,20 @@ class Espresso(Beverage):
     Extends also beverage class
     """
 
-    def __init__(self):
+    def __init__(self, _size):
         super().__init__()
         self.description = "Espresso"
+        self.size = _size
 
     def cost(self) -> float:
-        return 1.99
+        cost: float
+        if self.size == Size.Tall:
+            cost = 0.99
+        elif self.size == Size.Grande:
+            cost = 1.09
+        elif self.size == Size.Venti:
+            cost = 1.19
+        return cost
 
 
 class HouseBlend(Beverage):
@@ -50,9 +72,10 @@ class HouseBlend(Beverage):
     Extends also beverage class
     """
 
-    def __init__(self):
+    def __init__(self, _size):
         super().__init__()
         self.description = "House blend coffee"
+        self.size = _size
 
     def cost(self) -> float:
         return 0.89
@@ -63,9 +86,10 @@ class Decaf(Beverage):
     Extends also beverage class
     """
 
-    def __init__(self):
+    def __init__(self, _size):
         super().__init__()
         self.description = "Decaffinated Coffee"
+        self.size = _size
 
     def cost(self) -> float:
         return 1.09
@@ -76,9 +100,10 @@ class DarkRoast(Beverage):
     Extends also beverage class
     """
 
-    def __init__(self):
+    def __init__(self, _size):
         super().__init__()
         self.description = "Dark Roast Coffee"
+        self.size = _size
 
     def cost(self) -> float:
         return 0.99
@@ -100,7 +125,16 @@ class Mocha(CondimentDecorator):
         return self.beverage_that_condiment_wraps.get_description() + ", Mocha"
 
     def cost(self) -> float:
-        return self.beverage_that_condiment_wraps.cost() + 0.20
+        """This has to be repeated for all other drink classes"""
+        cost = self.beverage_that_condiment_wraps.cost()
+        drink_size = self.beverage_that_condiment_wraps.get_size()
+        if drink_size == Size.Tall:
+            cost += 0.20
+        elif drink_size == Size.Grande:
+            cost += 0.25
+        elif drink_size == Size.Venti:
+            cost += 0.30
+        return cost
 
 
 class SteamedMilk(CondimentDecorator):
@@ -155,16 +189,18 @@ class Whip(CondimentDecorator):
 
 
 if __name__ == '__main__':
-    my_beverage = Espresso()
+    my_beverage = Espresso(Size.Tall)
+    print("Beverage 1:" + my_beverage.get_description() + " €" + str(my_beverage.cost()))
+    my_beverage = Espresso(Size.Venti)
     print("Beverage 1:" + my_beverage.get_description() + " €" + str(my_beverage.cost()))
 
-    my_beverage2 = DarkRoast()
+    my_beverage2 = DarkRoast(Size.Venti)
     my_beverage2 = Mocha(my_beverage2)
     my_beverage2 = Mocha(my_beverage2)
     my_beverage2 = Whip(my_beverage2)
     print("Beverage 2:" + my_beverage2.get_description() + " €" + str(my_beverage2.cost()))
 
-    my_beverage3 = HouseBlend()
+    my_beverage3 = HouseBlend(Size.Venti)
     my_beverage3 = Soy(my_beverage3)
     my_beverage3 = Mocha(my_beverage3)
     my_beverage3 = Whip(my_beverage3)
